2fe62aca9cae68b4d73117ddafdc01497097a601
glckfndr <obulan65@gmail.com>
2024-01-05 18:20:07 +0200

    chore: Rename left to right

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -2,8 +2,8 @@ def merge_sort(arr)
   len = arr.length
   return arr if len <= 1
 
-  right = merge_sort(arr[0...len / 2])
-  left = merge_sort(arr[len / 2..])
+  left = merge_sort(arr[0...len / 2])
+  right = merge_sort(arr[len / 2..])
   merged = []
   merged << (left[0] < right[0] ? left.shift : right.shift) until right.empty? || left.empty?
   merged + left + right
--- spec/fibs_rec_spec.rb
+++ spec/fibs_rec_spec.rb
@@ -6,11 +6,11 @@ describe 'fibs_rec recursive method for Fibonacci Sequence' do
     expect(fibs_rec(8)).to eq([0, 1, 1, 2, 3, 5, 8, 13])
   end
 
-  it 'takes a 1 and returns an [0]' do
+  it 'takes a number 1 and returns an [0]' do
     expect(fibs_rec(1)).to eq([0])
   end
 
-  it 'takes a 2 and returns an [0, 1]' do
+  it 'takes a number 2 and returns an [0, 1]' do
     expect(fibs_rec(2)).to eq([0, 1])
   end
 end
